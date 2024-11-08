5b7d7228a3f5f337ddba1fcad91987f24f942270
CaoTranMinhTam0811 <22211tt2962@mail.tdc.edu.vn>
2024-11-08 10:05:46 +0700

    13-Pages-MinhTam

--- wp-content/themes/twentytwentyone/search.php
+++ wp-content/themes/twentytwentyone/search.php
@@ -284,46 +284,5 @@ else:
     get_template_part( 'template-parts/content/content-none' );
 endif;
 
-// Search result header
-?>
-<header class="page-header alignwide">
-    <h1 class="page-title">
-        <?php
-        printf(
-            /* translators: %s: Search term. */
-            esc_html__( 'Results for "%s"', 'twentytwentyone' ),
-            '<span class="page-description search-term">' . esc_html( get_search_query() ) . '</span>'
-        );
-        ?>
-    </h1>
-</header><!-- .page-header -->
-
-<div class="search-result-count default-max-width">
-    <?php
-    printf(
-        esc_html(
-            /* translators: %d: The number of search results. */
-            _n(
-                'We found %d result for your search.',
-                'We found %d results for your search.',
-                (int) $wp_query->found_posts,
-                'twentytwentyone'
-            )
-        ),
-        (int) $wp_query->found_posts
-    );
-    ?>
-</div><!-- .search-result-count -->
-<?php
-
-// Start the Loop for search results
-while ( have_posts() ) {
-    the_post();
-    get_template_part( 'template-parts/content/content-excerpt', get_post_format() );
-} // End the loop.
-
-// Previous/next page navigation.
-twenty_twenty_one_the_posts_navigation();
-
 get_footer();
 ?>
